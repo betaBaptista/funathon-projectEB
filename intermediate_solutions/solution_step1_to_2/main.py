6e1b6f8d4c814186919d1284f7b3d3dfd80ba00f
wzsa9j <nicolas.toulemonde@insee.fr>
2026-03-31 13:12:48 +0000

    rm duplicates

--- intermediate_solutions/solution_step1_to_2/main.py
+++ intermediate_solutions/solution_step1_to_2/main.py
@@ -6,8 +6,6 @@
 import polars as pl
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
 from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
 from sklearn.compose import ColumnTransformer, TransformedTargetRegressor
 from sklearn.pipeline import Pipeline
