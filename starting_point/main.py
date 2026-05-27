713280e9d08f99079d06512be6e4a7a327390004
elisabetebaptista <elisabete.baptista@ine.pt>
2026-05-27 10:31:06 +0000

    preprocessingEx1

--- starting_point/main.py
+++ starting_point/main.py
@@ -1,17 +1,23 @@
 # %%
 # ============================================
-# STEP 1 — Preprocessing
+# STEP 1 — Generate synthetic regression data
 # ============================================
-
 # YOUR CODE HERE
-print("teste")
-
+from sklearn.datasets import make_regression
+import pandas as pd
+X, y = make_regression(n_samples=300, n_features=3, noise=15, random_state=32)
+df = pd.DataFrame(X, columns=['X1', 'X2', 'X3'])
+df['y'] = y
+print(df.head(3))
 # %%
 # ============================================
-# STEP 2 — Train / test split, model fitting,
-#
+# STEP 2 — Preprocessing
 # ============================================
-
 # YOUR CODE HERE
-
+# %%
+# ============================================
+# STEP 3 — Train / test split, model fitting,
+#          and performance evaluation
+# ============================================
+# YOUR CODE HERE
 # %%
